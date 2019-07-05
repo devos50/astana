86ffa2aae09638ea30bf53a241517382964eb49b
Martijn de Vos <mdmartijndevos@gmail.com>
2019-07-05 16:27:11 +0200

    More work on decrypter

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,101 @@
+import com.googlecode.d2j.Field;
+import com.googlecode.d2j.Method;
 import com.googlecode.d2j.node.DexClassNode;
+import com.googlecode.d2j.node.DexCodeNode;
 import com.googlecode.d2j.node.DexMethodNode;
-import com.googlecode.d2j.node.insn.ConstStmtNode;
-import com.googlecode.d2j.node.insn.DexStmtNode;
-import com.googlecode.d2j.node.insn.MethodStmtNode;
+import com.googlecode.d2j.node.insn.*;
 import com.googlecode.d2j.reader.Op;
+import com.googlecode.d2j.smali.BaksmaliDumper;
 import com.googlecode.d2j.smali.Smali;
+import com.googlecode.d2j.smali.SmaliCmd;
+import com.googlecode.dex2jar.tools.Dex2jarCmd;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Main {
+import static com.googlecode.d2j.DexConstants.ACC_PUBLIC;
+import static com.googlecode.d2j.DexConstants.ACC_STATIC;
 
+public class Main {
+    private static String currentString = null;
+    private static int stringResultRegister = 0;
+    private static ArrayList<DexStmtNode> statements = new ArrayList<>();
+
+    private static void constructClass() throws IOException {
+        String[] paramTypes = {"[Ljava/lang/String;"};
+        Method m = new Method("LIsolated", "main", paramTypes, "V");
+        DexMethodNode mn = new DexMethodNode(ACC_PUBLIC | ACC_STATIC, m);
+        DexCodeNode cn = new DexCodeNode();
+        cn.stmts = statements;
+        mn.codeNode = cn;
+
+        // add the print statement and return-void
+        Field f = new Field("Ljava/lang/System;", "out", "Ljava/io/PrintStream;");
+        int printReg = (stringResultRegister == 0) ? 1 : 0;
+        FieldStmtNode fsn = new FieldStmtNode(Op.SGET_OBJECT, printReg, 0, f);
+        cn.stmts.add(fsn);
+
+        paramTypes = new String[]{"Ljava/lang/String;"};
+        Method printMethod = new Method("Ljava/io/PrintStream;", "println", paramTypes, "V");
+        int[] args = {printReg, stringResultRegister};
+        MethodStmtNode msn = new MethodStmtNode(Op.INVOKE_VIRTUAL, args, printMethod);
+        cn.stmts.add(msn);
+
+        Stmt0RNode returnVoidStmt = new Stmt0RNode(Op.RETURN_VOID);
+        cn.stmts.add(returnVoidStmt);
+        cn.visitRegister(20);
+
+        // create the class
+        DexClassNode classNode = new DexClassNode(ACC_PUBLIC, "LIsolated;", "Ljava/lang/Object;", null);
+        classNode.methods = new ArrayList<>();
+        classNode.methods.add(mn);
+
+        // convert to smali
+        BaksmaliDumper dumper = new BaksmaliDumper(false, true);
+        FileWriter fw = new FileWriter("temp/isolated.smali");
+        BufferedWriter log = new BufferedWriter(fw);
+        dumper.baksmaliClass(classNode, log);
+        try {
+            log.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // convert to .dex
+        new SmaliCmd().doMain("temp/Isolated.smali", "-o", "temp/isolated.dex");
+
+        // convert to .jar
+        new Dex2jarCmd().doMain("temp/isolated.dex", "--force", "-o", "temp/isolated.jar");
+
+        // run the jar
+        Process p = Runtime.getRuntime().exec("java -cp /Users/martijndevos/Downloads/barclays_original.jar:temp/isolated.jar:temp Isolated");
+        try {
+            int result = p.waitFor();
+            System.out.println("Process exit code: " + result);
+            System.out.println();
+            System.out.println("Result:");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            reader = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        currentString = null;
+        statements = new ArrayList<>();
+
+        // TODO: exit for now
+        //System.exit(0);
+    }
 
     public static void main(String[] args) throws IOException {
         InputStream input = new FileInputStream("data/ApplicationController.smali");
@@ -19,38 +103,37 @@ public class Main {
         for (Iterator<DexMethodNode> it = cn.methods.iterator(); it.hasNext(); ) {
             DexMethodNode m = it.next();
             System.out.println("Method: " + m.method.getName());
-            String curString = null;
-            ArrayList<DexStmtNode> statements = new ArrayList<DexStmtNode>();
-            for(Iterator<DexStmtNode> it2 = m.codeNode.stmts.iterator(); it2.hasNext(); ) {
-                DexStmtNode stmtNode = it2.next();
+            for(int stmtIndex = 0; stmtIndex < m.codeNode.stmts.size(); stmtIndex++ ) {
+                DexStmtNode stmtNode = m.codeNode.stmts.get(stmtIndex);
+                if(currentString != null) { statements.add(stmtNode); }
+
                 if(stmtNode.op == Op.CONST_STRING) {
-                    if(curString != null) {
+                    if(currentString != null) {
                         throw new RuntimeException("Already parsing string!");
                     }
 
                     ConstStmtNode cnn = (ConstStmtNode) stmtNode;
                     String stringValue = cnn.value.toString();
                     if(stringValue.length() > 0) {
-                        System.out.println("START");
-                        curString = stringValue;
+                        currentString = stringValue;
                         statements.add(stmtNode);
                     }
                 }
 
-                if(stmtNode.op == Op.INVOKE_DIRECT && curString != null) {
+                if(stmtNode.op == Op.INVOKE_DIRECT && currentString != null) {
                     MethodStmtNode mnn = (MethodStmtNode) stmtNode;
+                    stringResultRegister = mnn.args[0]; // first argument is always the string itself
                     if(mnn.method.getName().equals("<init>") && mnn.method.getOwner().equals("Ljava/lang/String;")) {
-                        curString = null;
-                        statements = new ArrayList<DexStmtNode>();
-                        System.out.println("STOP");
+                        constructClass();
                     }
                 }
-                else if(stmtNode.op == Op.INVOKE_STATIC && curString != null) {
+                else if(stmtNode.op == Op.INVOKE_STATIC && currentString != null) {
                     MethodStmtNode mnn = (MethodStmtNode) stmtNode;
                     if(mnn.method.getReturnType().equals("Ljava/lang/String;")) {
-                        curString = null;
-                        statements = new ArrayList<DexStmtNode>();
-                        System.out.println("STOP");
+                        Stmt1RNode nextStmtNode = (Stmt1RNode) m.codeNode.stmts.get(stmtIndex + 1);
+                        stringResultRegister = nextStmtNode.a;
+                        statements.add(nextStmtNode);
+                        constructClass();
                     }
 
                 }
